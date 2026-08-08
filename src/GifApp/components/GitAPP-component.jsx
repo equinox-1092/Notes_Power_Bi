3701d7d96a1c8fcf07f4dd499c9984c46461dbd8
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2026-08-08 11:30:04 -0500

    Delete src directory

--- src/GifApp/components/GitAPP-component.jsx
+++
@@ -1,8 +0,0 @@
-export const GitAPPComponent = ({ title, url, id }) => {
-  return (
-    <div className="card">
-      <img src={url} alt={title} />
-      <p>{title}</p>
-    </div>
-  );
-};
--- src/public/components/useLanguage.js
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from "react-i18next";
-
-export function useLanguage() {
-    const { i18n } = useTranslation();
-  const changeLanguage = (lang) => {
-    i18n.changeLanguage(lang);
-  };
-    return { changeLanguage };
-  }
